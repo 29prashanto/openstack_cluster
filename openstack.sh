15f1a549b2c9539cd3d96ea01097a9e446aac0f6
Prashanto Priyanshu <42933357+29prashanto@users.noreply.github.com>
2020-06-26 00:05:36 +0530

    first commit

---
+++ openstack.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+yum update -y
+
+yum install -y python3
+
+pip3 install gdown
+
+gdown   --id    1yJic7aVBKEWuZSxDvslRZOiuu70-DaAd   --output    rhel-7-server-additional-20180628.iso
+
+gdown   --id    15cKJFDij4Ecp1J1q5UeVbEEEYz8yT82U   --output    rhel-7.5-server-updates-20180628.iso
+
+gdown   --id    1iQKT5M4LduQxHjZPZlqydI2A7Z0bFicy   --output    RHEL7OSP-13.0-20180628.2-x86_64.iso
+
+mkdir /updates  /additional  /RHOSP
+
+mount -o loop rhel-7.5-server-updates-20180628.iso  /updates/
+
+mount -o loop rhel-7-server-additional-20180628.iso  /additional/
+
+mount -o loop RHEL7OSP-13.0-20180628.2-x86_64.iso /RHOSP/
+
+mkdir /RHOSP13
+
+cp -rvf /updates/  /RHOSP13/
+
+cp -rvf /additional/  /RHOSP13/
+
+cp -rvf /RHOSP/  /RHOSP13/
+
+yum install createrepo  -y
+
+createrepo -v /RHOSP13/.
+
+cat <<EOF > /etc/yum.repos.d/openstack.repo
+[Openstack]
+name=Openstack
+baseurl=file:///RHOSP13
+gpgcheck=0
+EOF
+
+yum install openstack-packstack -y
+
+packstack --gen-answer-file=setup.txt
+
+packstack  --answer-file=setup.txt
+
+
